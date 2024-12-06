40606c3db9f00f245a281c22d2075c542dcc1f54
Guy Moins <moinsg@helha.be>
2024-12-06 12:53:44 +0100

    Initial commit

---
+++ backend/demoBack/src/main/java/be/helha/projets/comptoirbinouze/moduleapi/demoback/DemoBackApplication.java
@@ -0,0 +1,13 @@
+package be.helha.projets.comptoirbinouze.moduleapi.demoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBackApplication.class, args);
+    }
+
+}
